6feaee6d2b643cfabef423d6d5d23530231a8507
nishapatel1212 <patelnisha107070@gmail.com>
2026-04-22 10:53:47 +1000

    changes

--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,29 @@ Route::get('/', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard', function () {
+        return view('admin_panel.dashboard');
+    });
+
+    // safetyCheck page
+    Route::prefix('safetycheck')->group(function () {
+        Route::get('/index', [SafetyCheckReportController::class, 'index'])->name('safetycheckreport.index');
+        Route::get('/create', [SafetyCheckReportController::class, 'create'])->name('safetycheckreport.create');
+        Route::post('/store', [SafetyCheckReportController::class, 'store'])->name('safetycheckreport.store');
+        Route::get('/edit/{id}', [SafetyCheckReportController::class, 'edit'])->name('safetycheckreport.edit');
+        Route::post('/update', [SafetyCheckReportController::class, 'update'])->name('safetycheckreport.update');
+        Route::post('/destroy/{id}', [SafetyCheckReportController::class, 'destroy'])->name('safetycheckreport.destroy');
+        Route::get('safetcheck-reports/{id}/pdf', [SafetyCheckReportController::class, 'downloadPdf'])->name('safetycheckreport.pdf');
+        Route::post('/inspection-items', [SafetyCheckReportController::class, 'addInspectionItem'])->name('inspection-items.store');
+        Route::post('visual-inspection-items/store', [SafetyCheckReportController::class, 'addVisualInspectionItem'])->name('visual-inspection-items.store');
+        Route::post('polarity-testing-items/store', [SafetyCheckReportController::class, 'addPolarityTestingItem'])->name('polarity-testing-items.store');
+        Route::post('earth-testing-items/store', [SafetyCheckReportController::class, 'addEarthTestingItem'])->name('earth-testing-items.store');
+    });
 
     // chat page
     Route::prefix('chatbot')->group(function () {
@@ -36,23 +55,6 @@ Route::middleware('auth')->group(function () {
     });
 });
 
-Route::get('/dashboard', function () {
-    return view('admin_panel.dashboard');
-})->middleware('auth');
 
-// safetyCheck page
-Route::middleware(['auth'])->prefix('safetycheck')->group(function () {
-    Route::get('/index', [SafetyCheckReportController::class, 'index'])->name('safetycheckreport.index');
-    Route::get('/create', [SafetyCheckReportController::class, 'create'])->name('safetycheckreport.create');
-    Route::post('/store', [SafetyCheckReportController::class, 'store'])->name('safetycheckreport.store');
-    Route::get('/edit/{id}', [SafetyCheckReportController::class, 'edit'])->name('safetycheckreport.edit');
-    Route::post('/update', [SafetyCheckReportController::class, 'update'])->name('safetycheckreport.update');
-    Route::post('/destroy/{id}', [SafetyCheckReportController::class, 'destroy'])->name('safetycheckreport.destroy');
-    Route::get('safetcheck-reports/{id}/pdf', [SafetyCheckReportController::class, 'downloadPdf'])->name('safetycheckreport.pdf');
-    Route::post('/inspection-items', [SafetyCheckReportController::class, 'addInspectionItem'])->name('inspection-items.store');
-    Route::post('visual-inspection-items/store', [SafetyCheckReportController::class, 'addVisualInspectionItem'])->name('visual-inspection-items.store');
-    Route::post('polarity-testing-items/store', [SafetyCheckReportController::class, 'addPolarityTestingItem'])->name('polarity-testing-items.store');
-    Route::post('earth-testing-items/store', [SafetyCheckReportController::class, 'addEarthTestingItem'])->name('earth-testing-items.store');
-});
 
 require __DIR__ . '/auth.php';
